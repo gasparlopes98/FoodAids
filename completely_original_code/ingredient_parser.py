efae6907e2d539d32d1e199f869f057c66ec8619
Guilherme-Mendes-ISEP <1190641@isep.ipp.pt>
2023-03-30 20:36:06 +0100

    sou muito bom, ele sugere comida

--- completely_original_code/ingredient_parser.py
+++ completely_original_code/ingredient_parser.py
@@ -1,16 +1,22 @@
-
 import pandas as pd
+import nltk
 import string
 import ast
 import re
 import unidecode
 
-
-# import config
+# nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from collections import Counter
 
 # Weigths and measures are words that will not add value to the model. I got these standard words from
 # https://en.wikibooks.org/wiki/Cookbook:Units_of_measurement
 
+# # We lemmatize the words to reduce them to their smallest form (lemmas).
+# lemmatizer = WordNetLemmatizer()
+# measures = [lemmatizer.lemmatize(m) for m in measures]
+# words_to_remove = [lemmatizer.lemmatize(m) for m in words_to_remove]
 
 
 def ingredient_parser(ingreds):
@@ -411,12 +417,13 @@ def ingredient_parser(ingreds):
     if isinstance(ingreds, list):
         ingredients = ingreds
     else:
-        ingredients = ast.literal_eval(ingreds)
+        ingredients = ingreds.split(",")
     # We first get rid of all the punctuation. We make use of str.maketrans. It takes three input
     # arguments 'x', 'y', 'z'. 'x' and 'y' must be equal-length strings and characters in 'x'
     # are replaced by characters in 'y'. 'z' is a string (string.punctuation here) where each character
     #  in the string is mapped to None.
     translator = str.maketrans("", "", string.punctuation)
+    lemmatizer = WordNetLemmatizer()
     ingred_list = []
     for i in ingredients:
         i.translate(translator)
@@ -430,10 +437,13 @@ def ingredient_parser(ingreds):
         items = [
             unidecode.unidecode(word) for word in items
         ]  #''.join((c for c in unicodedata.normalize('NFD', items) if unicodedata.category(c) != 'Mn'))
-        
+        # Lemmatize words so we can compare words to measuring words
+        items = [lemmatizer.lemmatize(word) for word in items]
+        # Gets rid of measuring words/phrases, e.g. heaped teaspoon
         items = [word for word in items if word not in measures]
         # Get rid of common easy words
         items = [word for word in items if word not in words_to_remove]
+
         if items:
             ingred_list.append(" ".join(items))
     # ingred_list = " ".join(ingred_list)
@@ -441,16 +451,28 @@ def ingredient_parser(ingreds):
 
 
 if __name__ == "__main__":
-    recipe_df = pd.read_csv(config.RECIPES_PATH)
+    recipe_df = pd.read_csv("leiria.csv")
     recipe_df["ingredients_parsed"] = recipe_df["ingredients"].apply(
         lambda x: ingredient_parser(x)
     )
-    df = recipe_df[["recipe_name", "ingredients_parsed", "ingredients", "recipe_urls"]]
-    df = recipe_df.dropna()
+
+    df = recipe_df[["title","ingredients","ingredients_parsed"]]
 
     # remove - Allrecipes.com from end of every recipe title
-    m = df.recipe_name.str.endswith("Recipe - Allrecipes.com")
-    df["recipe_name"].loc[m] = df.recipe_name.loc[m].str[:-23]
-    df.to_csv(config.PARSED_PATH, index=False)
+ 
+    df.to_csv("parsed.csv", index=False)
 
-    
+
+    # vocabulary = nltk.FreqDist()
+    # for ingredients in recipe_df['ingredients']:
+    #     ingredients = ingredients.split()
+    #     vocabulary.update(ingredients)
+
+    # for word, frequency in vocabulary.most_common(200):
+    #     print(f'{word};{frequency}')
+    # fdist = nltk.FreqDist(ingredients)
+
+    # common_words = []
+    # for word, _ in vocabulary.most_common(250):
+    #     common_words.append(word)
+    # print(common_words)
