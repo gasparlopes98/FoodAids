9135d0fbdfe98f1b78acad54fd0590cab3981f64
Goncalo Lopes <gasparlopes98gmail.com>
2023-04-13 20:48:18 +0100

    Script recipe to ingredients

--- tratar_csv.py
+++ tratar_csv.py
@@ -1,23 +1,22 @@
 import csv
 import pandas as pd
-max=1000
+max=10000
 
 data=[]
 
-the_file = pd.read_csv("RecipeNLG_dataset.csv")
+the_file = pd.read_csv("csv_file/Food_Recipes.csv",delimiter=',')
 
-the_file.drop('ingredients', inplace=True, axis=1)
-the_file.drop('link', inplace=True, axis=1)
-the_file.drop('source', inplace=True, axis=1)
+the_file.drop('Image_Name', inplace=True, axis=1)
+the_file.drop('Cleaned_Ingredients', inplace=True, axis=1)
+# the_file.drop('source', inplace=True, axis=1)
 
 # row_count = sum(1 for row in the_file)
 
-for row in range(max):
-    data.append(the_file.loc[row])
+data = the_file[:max]
+data.drop('Unnamed: 0', inplace=True, axis=1)
     
-
-# the_file.close()  
-
-with open('new_recipes.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(data)  
+print(data)
+data.to_csv('csv_file/new_recipes.csv', index=False)
+# with open('new_recipes.csv', 'w', newline='') as file:
+#     writer = csv.writer(file)
+#     writer.writerows(data)
